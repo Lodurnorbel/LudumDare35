916934fcf172574727568d4e2043830101df60e6
reignerok <reignerok@gmail.com>
2016-04-16 05:46:30 +0200

    Added basic health system. Enemies (with tag Enemy) cause damage to the player on touch

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public int startingHealth = 100;
+
+	private bool isDamaged;
+	private int currentHealth;
+
+	// Use this for initialization
+	void Start () {
+		currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isDamaged) {
+			ChangeHealthBy (-10);
+			isDamaged = false;
+		}
+	}
+
+	void ChangeHealthBy(int quantity) {
+		currentHealth += quantity;
+		Debug.Log ("Current health: " + currentHealth);
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.CompareTag ("Enemy")) {
+			isDamaged = true;
+		}
+	}
+
+
+}
