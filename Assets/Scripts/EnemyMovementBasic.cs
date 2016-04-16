1c868acd85002d89002f2bd34008b14e51679827
reignerok <reignerok@gmail.com>
2016-04-16 06:10:13 +0200

    Improved EnemyMovementBasic. Now it turns around when collides on walls

--- Assets/Scripts/EnemyMovementBasic.cs
+++ Assets/Scripts/EnemyMovementBasic.cs
@@ -6,16 +6,21 @@ public class EnemyMovementBasic : MonoBehaviour {
 	public float speed = 5.0f;
 
 	private Rigidbody2D rigidBody;
+	private int sense;
 
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> ();
+		sense = -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (rigidBody.velocity.x == 0) {
+			sense *= -1;
+		}
 		Vector2 newVelocity = rigidBody.velocity;
-		newVelocity.x = -speed;
+		newVelocity.x = sense*speed;
 		rigidBody.velocity = newVelocity;
 	}
 }
