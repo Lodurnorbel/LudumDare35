c2faeb8be29618e9e84faf339513d519aaf40bfe
reignerok <reignerok@gmail.com>
2016-04-16 05:18:08 +0200

    Added basic enemy movement

---
+++ Assets/EnemyMovementBasic.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovementBasic : MonoBehaviour {
+
+	public float speed = 5.0f;
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newVelocity = rigidBody.velocity;
+		newVelocity.x = -speed;
+		rigidBody.velocity = newVelocity;
+	}
+}
