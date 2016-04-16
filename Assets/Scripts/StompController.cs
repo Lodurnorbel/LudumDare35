7637c5cd0f346dd5179e37fbb98acc4b9f1c74ee
reignerok <reignerok@gmail.com>
2016-04-16 19:00:55 +0200

    Added stomp killing of enemies.

---
+++ Assets/Scripts/StompController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StompController : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.gameObject.CompareTag ("Player")) {
+			GetComponentInParent<EnemyController> ().Die ();
+		}
+	}
+}
