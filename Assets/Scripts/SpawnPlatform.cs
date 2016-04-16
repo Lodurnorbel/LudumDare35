e56bd2d82aeefacafed2b7a15f4f92d299698532
Adolfo Ingelmo <Adolfo Ingelmo>
2016-04-16 15:05:38 +0200

    Auto generated map :D

--- Assets/Scripts/SpawnPlatform.cs
+++ Assets/Scripts/SpawnPlatform.cs
@@ -4,12 +4,13 @@ using System.Collections;
 public class SpawnPlatform : MonoBehaviour {
 
     public GameObject[] obj;
+    public GameObject next;
     public float tiempoMin = 1f;
     public float tiempoMax = 2f;
 
 	// Use this for initialization
 	void Start () {
-        Generar();
+        /*NotificationCenter.DefaultCenter().AddObserver(this, "PersonajeAnda");*/
 	}
 	
 	// Update is called once per frame
@@ -19,7 +20,16 @@ public class SpawnPlatform : MonoBehaviour {
 
     void Generar() {
         Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
-        Invoke("Generar", Random.Range(tiempoMin, tiempoMax));
+        /*Invoke("Generar", Random.Range(tiempoMin, tiempoMax));*/
     }
+
+
+    void OnTriggerEnter2D (Collider2D c)
+    {
+        SpawnPlatform a = (SpawnPlatform)next.GetComponent(typeof(SpawnPlatform));
+        a.Generar();
+        Debug.Log("Nuevo Terreno");
+    }
+
 
 }
