d43ac6665343427fbb4b2417eb43e2aaf970ac9c
Adolfo Ingelmo <Adolfo Ingelmo>
2016-04-17 23:57:09 +0200

    Animations for everyone! Well, not for everyone, for the 70% of the characters.

--- Assets/Scripts/InterfaceManagement.cs
+++ Assets/Scripts/InterfaceManagement.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InterfaceManagement : MonoBehaviour {
 
@@ -21,13 +22,14 @@ public class InterfaceManagement : MonoBehaviour {
     void Start () {
         pm = player.GetComponent<PlayerMovement>();
         ph = player.GetComponent<PlayerHealth>();
-        blood = 90;
+        blood = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
         barDisplay = ph.currentHealth * 0.01f;
         barDisplay2 = blood * 0.01f;
+        GameObject.FindGameObjectWithTag("Score").GetComponent<Text>().text = "SCORE: " + GameObject.FindGameObjectWithTag("ScoreSystem").GetComponent<ScoreSystem>().getScore();
 
         if (Input.GetKeyDown("1")){
             pm.setCriature(0);
