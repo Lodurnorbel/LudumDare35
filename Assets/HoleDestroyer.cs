772206462ab56331a5959c12106c039182153c68
reignerok <reignerok@gmail.com>
2016-04-18 21:36:59 +0200

    Added destroyer for holes

---
+++ Assets/HoleDestroyer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoleDestroyer : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.CompareTag ("Player")) {
+			coll.GetComponentInParent<PlayerController> ().Die ();
+		} else {
+			Destroy (coll.gameObject);
+		}
+	}
+}
